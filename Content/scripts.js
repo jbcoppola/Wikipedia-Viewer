66f733ed2e99c7811acf68ac75339a427ea80d29
Joseph Coppola <jbcoppola@gmail.com>
2017-10-27 16:20:16 -0700

    added footer to scroll to top

--- Content/scripts.js
+++ Content/scripts.js
@@ -12,15 +12,22 @@
 
 function searchFunc(url) {
     if (document.querySelector(".results-list").classList.contains("expand")) {
+        document.querySelector(".footer").classList.add("hidden");
         retract();
         setTimeout(function () {
             buttonGet(url);
             expand();
         }, 1000);
+        setTimeout(function () {
+            document.querySelector(".footer").classList.remove("hidden");
+        }, 2000);
     }
     else {
         buttonGet(url);
         expand();
+        setTimeout(function () {
+            document.querySelector(".footer").classList.remove("hidden");
+        }, 800);
     }
 }
 
@@ -32,12 +39,12 @@ function buttonGet(url) {
             return response;
         }
         throw new Error('Network response was not ok: ' + response.statusText);
-        }).then(function (data) {
-            return data.json();
-        }).then(function (data) {
-            var results = data.query.pages;
-            document.querySelector(".results-list").innerHTML = htmlBuilder(results);
-        })
+    }).then(function (data) {
+        return data.json();
+    }).then(function (data) {
+        var results = data.query.pages;
+        document.querySelector(".results-list").innerHTML = htmlBuilder(results);
+    })
         .catch(function (error) {
             console.log(error);
         });
@@ -48,12 +55,19 @@ function htmlBuilder(pages) {
     for (page in pages) {
         if (pages[page].terms.description) {
             var desc = pages[page].terms.description[0];
-            var display = "string: '" + desc + "'. Found disambiguation: " + !(desc.indexOf("disambiguation") === -1);
             if (desc.indexOf("disambiguation") === -1) {
                 var result = '<a href="http://en.wikipedia.org/?curid=' + pages[page].pageid + '" target="_blank" class="result">';
                 result += "<h1>" + pages[page].title + "</h1>";
-                if (pages[page].original && pages[page].title.length < 30) { result += '<img src="' + pages[page].original.source + '" />'; }
-                result += "<p>" + pages[page].terms.description + "</p>";
+
+                //truncate description if too long
+                var maxLen = 100;
+                if (desc.length >= maxLen) { desc = desc.slice(0, maxLen) + "..."; }
+
+                //only include image if title and description aren't long
+                if (pages[page].original && pages[page].title.length < 30 && desc.length < maxLen) {
+                    result += '<img src="' + pages[page].original.source + '" />';
+                }
+                result += "<p>" + desc + "</p>";
                 result += "</a>";
                 htmlString += result;
             }
